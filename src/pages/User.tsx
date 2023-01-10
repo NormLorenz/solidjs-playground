000a135c06d6d243eee3563b8c98bdd3f2a451c7
NormLorenz <normlorenz@gmail.com>
2023-01-10 09:49:51 -0800

    Update User.tsx

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -12,26 +12,26 @@ const User = () => {
       <form>
 
         <div class="row">
-          <div class="col d-flex justify-content-center border">
+          <div class="col d-flex justify-content-center">
             <h1>Star War's User Page</h1>
           </div>
         </div>
 
         <div class="row">
-          <div class="col-3 border"></div>
+          <div class="col-3"></div>
           <div class="col-6">
             <label for="inputFormControl" class="form-label">User's number:</label>
             <input type="number" min="1" max="75" class="form-control" id="inputFormControl" placeholder="A number between 1 ... 75"
               onInput={(e) => setUserId(e.currentTarget.value)} />
           </div>
-          <div class="col-3 border"></div>
-          <div class="col-3 border"></div>
+          <div class="col-3"></div>
+          <div class="col-3"></div>
           <div class="col-6">
             <label for="outputFormControl" class="form-label">Result:&nbsp;</label>
             <Show when={!user.loading} fallback={<div class="spinner-border spinner-border-sm" role="status"></div>}> </Show>
             <textarea class="form-control" value={JSON.stringify(user(), null, 2)} id="outputFormControl" rows="18"></textarea>
           </div>
-          <div class="col-3 border"></div>
+          <div class="col-3"></div>
         </div>
 
       </form>
