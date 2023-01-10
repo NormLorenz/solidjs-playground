715383d6952bf65937088b49658d56826dee5dd9
NormLorenz <normlorenz@gmail.com>
2023-01-10 09:45:20 -0800

    Update Error.tsx

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -4,19 +4,19 @@ const Error = () => {
     <div class="container">
 
       <div class="row pt-2">
-        <div class="col d-flex justify-content-center border">
+        <div class="col d-flex justify-content-center">
           <h1>404 Error Page</h1>
         </div>
       </div>
 
       <div class="row pt-2">
-        <div class="col d-flex justify-content-center border">
+        <div class="col d-flex justify-content-center">
           The page you requested doesn't exist.
         </div>
       </div>
 
       <div class="row pt-2">
-        <div class="col d-flex justify-content-center border">
+        <div class="col d-flex justify-content-center">
           <a href="#" class="btn btn-outline-primary">Go to home</a>
         </div>
       </div>
