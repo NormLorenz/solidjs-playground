b72fe14735dbbaf3f4c669b0062198e47d59610f
NormLorenz <normlorenz@gmail.com>
2023-01-11 09:10:23 -0800

    Create bookShelf.state.ts

---
+++ src/pages/Bookshelf/bookShelf.state.ts
@@ -0,0 +1,14 @@
+import { createSignal, createMemo, createRoot } from "solid-js";
+
+const bookShelfState = () => {
+
+  // const [count, setCount] = createSignal(0);
+
+  // const increment = () => setCount(count() + 1);
+  // const clear = () => setCount(0);
+  // const doubleCount = createMemo(() => count() * 2);
+
+  // return { count, doubleCount, increment, clear };
+}
+
+export default createRoot(bookShelfState);
