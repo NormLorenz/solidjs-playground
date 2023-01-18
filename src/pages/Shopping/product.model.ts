f2f8046797058c70aca80d0e1bf96388cc459d5c
NormLorenz <normlorenz@gmail.com>
2023-01-18 12:48:25 -0800

    Create product.model.ts

---
+++ src/pages/Shopping/product.model.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: string;
+  description: string;
+  name: string;
+  price: number;
+  currency: string;
+  image: string;
+  slug: string;
+  category: string[];
+  sizes: string[];
+};
