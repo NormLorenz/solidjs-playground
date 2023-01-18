12aab19f14dbb9a193939658ed352ccaa45219aa
NormLorenz <normlorenz@gmail.com>
2023-01-18 12:49:16 -0800

    Update productList.ts

---
+++ src/pages/Shopping/productList.ts
@@ -0,0 +1,69 @@
+import { Product } from "./product.model";
+
+export const productList: Product[] = [
+  {
+    id: '3e4374a5-0a54-4278-b611-feb69e9dbc21',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    name: 'Black Jacket',
+    price: 50,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1592878849122-facb97520f9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'black-jacket',
+    category: ['Formal', 'Casual'],
+    sizes: ['Small', 'Medium', 'Large']
+  },
+  {
+    id: '6d22ea13-f41d-4b7c-8674-ae91876f47bf',
+    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore',
+    name: 'Green Jacket',
+    price: 70,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1592878940526-0214b0f374f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'green-jacket',
+    category: ['Flashy'],
+    sizes: ['Small', 'Medium', 'Large']
+  },
+  {
+    id: '500ff4e3-e0e9-4c73-b135-daa45b4e250d',
+    description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
+    name: 'Striped Jacket',
+    price: 45,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1594938298603-c8148c4dae35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'striped-jacket',
+    category: ['Casual', 'Sporty'],
+    sizes: ['Small', 'Medium', 'Large']
+  },
+  {
+    id: '80d95a87-141d-4129-b736-ee9b58f8a2af',
+    description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint',
+    name: 'Blue Jacket',
+    price: 65,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1593030942428-a5451dca4b42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'blue-jacket',
+    category: ['Sporty'],
+    sizes: ['Small', 'Medium', 'Large']
+  },
+  {
+    id: 'f27f288c-c6cb-43b0-9f2a-d575ad37fe85',
+    description: 'Delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+    name: 'Fuzzy Jacket',
+    price: 35,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1594938374182-a57061dac3df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'fuzzy-jacket',
+    category: ['Casual'],
+    sizes: ['Small', 'Medium', 'Large']
+  }, {
+    id: '6b0f2089-b3f7-4c18-9b88-5f2e6f23c8bb',
+    description: 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis',
+    name: 'Shiny Jacket',
+    price: 35,
+    currency: 'USD',
+    image: 'https://images.unsplash.com/photo-1594938328870-9623159c8c99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80',
+    slug: 'shiny-jacket',
+    category: ['Flashy', 'Formal'],
+    sizes: ['Small', 'Medium', 'Large']
+  },
+]
